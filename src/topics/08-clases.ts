c325ae9dbae8ab918aa291def1b587b5f3a17b70
Lizeth castillo <64451121+lizethcas@users.noreply.github.com>
2024-06-26 13:24:40 -0500

    define clases long way in TS

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -1,3 +1,14 @@
 export class Person {
-  
+  public name: string;
+  private address: string;
+
+  constructor() {
+    this.name = "Lizeth";
+    this.address = "Medellín";
+  }
 }
+
+const ironman = new Person();
+console.log(ironman);
+
+//el constructor es un metodo especial de una clase, es el primer metodo que se va llamar cuando se crea la instancio de una clase
